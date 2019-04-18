14d905b16a2a6ee43bd4429b6f8a8d164dd79383
Charles Houston <charles.houston11@imperial.ac.uk>
2019-04-18 11:38:12 +0100

    simplifying notebook for ease of use

--- transform_functions.py
+++ transform_functions.py
@@ -1,15 +1,17 @@
 import numpy as np
 from typing import List
 
-def calculate_rotation(ref1: List[int],
-                       ref2: List[int]):
+def calculate_rotation(ref1: List[float],
+                       ref2: List[float]) -> float:
   theta = (np.arctan2(ref1[1], ref1[0]) -
            np.arctan2(ref2[1], ref2[0]))
+  print('The dish has been rotated %.3f degrees' %
+        np.rad2deg(theta))
   return theta
 
 
-def rotate_pt(pt: List[int],
-              theta: float):
+def rotate_pt(pt: List[float],
+              theta: float) -> List[float]:
   magnitude = np.linalg.norm(pt)
   alpha = np.arctan2(pt[1],pt[0]) - theta
   new_pt = ([
@@ -17,3 +19,13 @@ def rotate_pt(pt: List[int],
     magnitude*np.sin(alpha)
   ])
   return new_pt
+
+def transform_coords(locs: List[List[float]],
+                     theta: float) -> List[List[float]]:
+  locs1 = np.array(locs)
+  locs2 = np.zeros(locs1.shape)
+  for i, pt in enumerate(locs1):
+    locs2[i,:] = rotate_pt(pt, theta)
+  print('The transformed coordinates are:')
+  print(locs2)
+  return locs2
